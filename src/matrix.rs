ebe078c4dce68e3c98fe609a2378c9503d3ca76c
mar.salvalaggio@gmail.com <55287795+Salva32@users.noreply.github.com>
2023-06-05 21:03:46 +0200

    wip

--- src/matrix.rs
+++ src/matrix.rs
@@ -14,7 +14,6 @@ impl Matrix {
 
     #[staticmethod]
     pub fn zeros(dimensions: (usize, usize)) -> Self {
-
         let (rows, cols) = dimensions;
         let data: Vec<Vec<f64>> = vec![vec![0.0; cols]; rows];
 
@@ -23,16 +22,13 @@ impl Matrix {
 
     #[staticmethod]
     pub fn ones(dimensions: (usize, usize)) -> Self {
-
         let (rows, cols) = dimensions;
         let data: Vec<Vec<f64>> = vec![vec![1.0; cols]; rows];
-
         Matrix { data }
     }
 
     #[staticmethod]
     pub fn eye(size: usize) -> Self {
-
         let data: Vec<Vec<f64>> = (0..size).map(|i| {
             (0..size).map(|j| if i==j {1.0} else {0.0}).collect()
         }).collect();
@@ -41,26 +37,27 @@ impl Matrix {
     }
 
     #[staticmethod]
-    pub fn matrix(data: Vec<Vec<f64>>) -> Self {
+    pub fn full(dimensions: (usize, usize), fill_value: f64) -> Self {
+        let (rows, cols) = dimensions;
+        let data: Vec<Vec<f64>> = vec![vec![fill_value; cols]; rows];
+        Matrix { data }
+    }
 
+    #[staticmethod]
+    pub fn matrix(data: Vec<Vec<f64>>) -> Self {
         Matrix { data } 
     }
-    
 
     #[getter]
     pub fn shape(&self) -> (usize, usize) {
-
         let rows = self.data.len();
         let cols = self.data[0].len();
-
         (rows, cols)
     }
 
     pub fn __add__(&self, other: &Matrix) -> Matrix {
-
         assert_eq!(self.data.len(), other.data.len());
         assert_eq!(self.data[0].len(), other.data[0].len());
-
         let result: Vec<Vec<f64>> = self
             .data
             .par_iter()
@@ -71,26 +68,21 @@ impl Matrix {
                     .map(|(elem1, elem2)| elem1 + elem2)
                     .collect()
             }).collect();
-
         Matrix { data: result }
     }
 
     pub fn __neg__(&self) -> Matrix {
-
         let result: Vec<Vec<f64>> = self
             .data
             .par_iter()
             .map(|row| row.iter().map(|elem| -*elem).collect())
             .collect();
-
         Matrix { data: result }
     }
 
     pub fn __sub__(&self, other: &Matrix) -> Matrix {
-
         assert_eq!(self.data.len(), other.data.len());
         assert_eq!(self.data[0].len(), other.data[0].len());
-
         let result: Vec<Vec<f64>> = self
         .data
         .par_iter()
@@ -101,7 +93,6 @@ impl Matrix {
                 .map(|(elem1, elem2)| elem1 - elem2)
                 .collect()
         }).collect();
-
         Matrix { data: result }
     }
 
@@ -109,58 +100,8 @@ impl Matrix {
         self.data.clone()
     }
 
-    // pub fn __getitem__(&self, index: usize) -> PyResult<Vec<f64>> {
-    //     match self.data.get(index) {
-    //         Some(row) => Ok(row.clone()),
-    //         None => Err(pyo3::exceptions::PyIndexError::new_err("Index out of range")),
-    //     }
-    // }
-
-    // pub fn __getitem__(&self, index: Vec<usize>) -> PyResult<Matrix> {
-        
-    //     match index.len() {
-    //         1 => {
-    //             let i = index[0];
-    //             match self.data.get(i) {
-    //                 Some(row) => Ok(Matrix::matrix(vec![row.clone()])),
-    //                 None => Err(pyo3::exceptions::PyIndexError::new_err("Index out of range")),
-    //             }
-    //         },
-    //         2 => {
-    //             let start = index[0];
-    //             let stop = index[1];
-    //             let step = 1;
-    //             let mut result = Vec::new();
-    //             for i in (start..stop).step_by(step) {
-    //                 match self.data.get(i) {
-    //                     Some(row) => result.push(row.clone()),
-    //                     None => return Err(pyo3::exceptions::PyIndexError::new_err("Index out of range")),
-    //                 }
-    //             }
-    //             Ok(Matrix::matrix(result))
-    //         },
-    //         3 => {
-    //             let start = index[0];
-    //             let stop = index[1];
-    //             let step = index[2];
-    //             let mut result = Vec::new();
-    //             for i in (start..stop).step_by(step) {
-    //                 match self.data.get(i) {
-    //                     Some(row) => result.push(row.clone()),
-    //                     None => return Err(pyo3::exceptions::PyIndexError::new_err("Index out of range")),
-    //                 }
-    //             }
-    //             Ok(Matrix::matrix(result))
-    //         },
-    //         _ => Err(pyo3::exceptions::PyTypeError::new_err("Invalid index")),
-    //     }
-    // }
-
-
     pub fn __repr__(&self) -> String {
-
         let mut result = String::new();
-    
         for (i, row) in self.data.iter().enumerate() {
             if i == 0 {
                 result.push_str("Matrix([");
@@ -179,7 +120,6 @@ impl Matrix {
                 result.push_str("])");
             }
         }
-        
         result
     }    
 
